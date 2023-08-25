a567c0807dd7f76436fac2740cf6a5e04e424502
zerrty <pgrytsyshyn@gmail.com>
2023-08-26 02:16:34 +0300

    added translation and MPTT model, gave some styles

--- MultiLevelMenu/MultiLevelMenu/urls.py
+++ MultiLevelMenu/MultiLevelMenu/urls.py
@@ -14,10 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + i18n_patterns(
+    path("i18n/", include("django.conf.urls.i18n")),
+    path('', include('main.urls')),
+)
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', include('main.urls')),
+# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
